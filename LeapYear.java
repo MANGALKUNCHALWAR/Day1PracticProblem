12b5e51feb197f1046ac36bb25c496a63edbfdb2
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-11 18:47:19 +0530

    LeapYear

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+package day1.practic.problems;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a year");
+        int year = scanner.nextInt();
+        if (year % 4 == 0) {
+            System.out.println("year is leap year");
+        } else {
+            System.out.println("year is not a leap year");
+        }
+    }
+}
